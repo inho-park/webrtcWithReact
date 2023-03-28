8fbc14c6d0f26c3bc7a9b35e94dae8efbd6ebad6
JunSeong_Park <ttotto0608@naver.com>
2023-03-28 12:43:13 +0900

    webcam call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useRef, useEffect } from "react";
 import './App.css';
 
 function App() {
+  
+  // 개인의 웹캠 화면
+  const localVideoRef = useRef();
+  // 상대방의 웹캠 화면
+  const remoteVideoRef = useRef();
+
+  useEffect(() => {
+    const constraints = {
+      audio: true,
+      video: true
+    };
+
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(stream => {
+        localVideoRef.current.srcObject = stream;
+      }).catch(e => {
+      console.log("getUserMedia Error : ", e);
+    });
+  }, []);
+
+  const getUserMedia = () => {
+    // const constraints = {
+    //   audio: true,
+    //   video: true
+    // };
+    //
+    //
+    // navigator.mediaDevices.getUserMedia(constraints)
+    //   .then(stream => {
+    //     localVideoRef.current.srcObject = stream;
+    //   }).catch(e => {
+    //     console.log("getUserMedia Error : ", e);
+    //   });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 10 }}>
+      <video ref={localVideoRef} autoPlay style={{
+          width: 640,
+          height: 480,
+          margin: 5,
+          backgroundColor: "black"
+      }}></video>
+      <video ref={remoteVideoRef} autoPlay style={{
+        width: 640,
+        height: 480,
+        margin: 5,
+        backgroundColor: "black"
+      }}></video>
+
+      <br/>
+      <button onClick={() => {
+
+      }}>Create Offer</button>
+      <button onClick={() => {
+
+      }}>Create Answer</button>
+      <br/>
+      <button onClick={() => {
+
+      }}>Set Remote Description</button>
+      <button onClick={() => {
+
+      }}>Add Candidate</button>
+
     </div>
   );
 }
