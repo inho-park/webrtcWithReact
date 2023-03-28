2ed96417d46ce7202eec3d94d501ca195d1323f7
JunSeong_Park <ttotto0608@naver.com>
2023-03-28 13:13:17 +0900

    add create offer and answer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   // 상대방의 웹캠 화면
   const remoteVideoRef = useRef();
 
+  //
+  const pc = useRef(new RTCPeerConnection(null));
+
+
   useEffect(() => {
     const constraints = {
       audio: true,
@@ -21,22 +25,52 @@ function App() {
       }).catch(e => {
       console.log("getUserMedia Error : ", e);
     });
+
+    // peer connection 변수 생성
+    const _pc = new RTCPeerConnection(null);
+    _pc.onicecandidate = (e) => {
+      if (e.candidate) console.log(JSON.stringify(e.candidate));
+    }
+
+    _pc.oniceconnectionstatechange = (e) => {
+      console.log(e)
+    }
+
+    _pc.ontrack = (e) => {
+      // we got remote stream...
+
+    }
+
+    pc.current = _pc;
   }, []);
 
-  const getUserMedia = () => {
-    // const constraints = {
-    //   audio: true,
-    //   video: true
-    // };
-    //
-    //
-    // navigator.mediaDevices.getUserMedia(constraints)
-    //   .then(stream => {
-    //     localVideoRef.current.srcObject = stream;
-    //   }).catch(e => {
-    //     console.log("getUserMedia Error : ", e);
-    //   });
-  };
+  // offer 로 먼저 sdp 프로토콜 제안 보내기
+  // offer 보낼 시 ICE ( interact connectivity establishment ) candidate 를
+  // peer connection 에 추가할 때 사용가능
+  const createOffer = () => {
+    pc.current.createOffer({
+      offerToReceiveAudio: 1,
+      offerToReceiveVideo: 1
+    }).then(sdp => {
+      console.log(JSON.stringify(sdp));
+      pc.current.setLocalDescription(sdp);
+    }).catch( e => console.log("createOffer error : " + e))
+  }
+
+  // answer 로 sdp 프로토콜 제안 응답하기
+  const createAnswer = () => {
+    pc.current.createOffer({
+      offerToReceiveAudio: 1,
+      offerToReceiveVideo: 1
+    }).then(sdp => {
+      console.log(JSON.stringify(sdp));
+      pc.current.setLocalDescription(sdp);
+    }).catch( e => console.log("createOffer error : " + e))
+  }
+
+  const setRemoteDescription = () => {
+
+  }
 
   return (
     <div style={{ margin: 10 }}>
@@ -50,23 +84,24 @@ function App() {
         width: 640,
         height: 480,
         margin: 5,
-        backgroundColor: "black"
+        backgroundColor: "gray"
       }}></video>
 
       <br/>
-      <button onClick={() => {
+      <button onClick={createOffer}>
+        Create Offer
+      </button>
+      <button onClick={createAnswer}>
+        Create Answer
+      </button>
+<br/>
+      <button onClick={createOffer}>
+        Set Remote Description
+      </button>
+      <button onClick={createOffer}>
+        Add Candidates
+      </button>
 
-      }}>Create Offer</button>
-      <button onClick={() => {
-
-      }}>Create Answer</button>
-      <br/>
-      <button onClick={() => {
-
-      }}>Set Remote Description</button>
-      <button onClick={() => {
-
-      }}>Add Candidate</button>
 
     </div>
   );
